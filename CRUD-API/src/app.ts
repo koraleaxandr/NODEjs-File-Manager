9f9f7d5e501f09812c06a8b944acd5b367cc77e7
koraleaxandr <koraleaxandr@gmail.com>
2022-06-20 13:37:18 +0300

    fix: temp save

--- CRUD-API/src/app.ts
+++ CRUD-API/src/app.ts
@@ -4,8 +4,10 @@ const PORT: number = 3000;
 
 const server = http
   .createServer( (request, res) => {
-    res.setHeader("Content-type", "application/json");
+    res.setHeader('Content-type', 'application/json');
     res.write('<h1>HELLO</h1>');
+    
+    
     const data = JSON.stringify([
     { name: 'Alex', age: 22}
     ]);
@@ -13,8 +15,8 @@ const server = http
     console.log('Server request:');
     console.log(request.url, request.method);    
   })
-  .listen(PORT, '127.0.0.1', (error: Error) => {
-    error ? console.error(error) : console.log(
+  .listen(PORT, (): void => {
+     console.log(
       `Server listening port: ${PORT}`
     );
   })
